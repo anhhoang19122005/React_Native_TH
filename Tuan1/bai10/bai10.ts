dcefb136c2607ddd8410bfad58edc0277d3deeeb
H82M19\Student <Student>
2026-08-23 16:45:41 +0700

    add: bai 8 9 10

---
+++ Tuan1/bai10/bai10.ts
@@ -0,0 +1,11 @@
+class Account {
+    private username: string;
+    private password: string;
+    readonly createdAt: Date;
+
+    constructor(username: string, password: string, createdAt: Date) {
+        this.username = username;
+        this.password = password;
+        this.createdAt = createdAt;
+    }
+}
