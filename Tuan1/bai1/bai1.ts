a823419fffdd282a50db1a2dc26549c5898de6a4
H82M19\Student <Student>
2026-08-23 15:40:40 +0700

    add: bai1

---
+++ Tuan1/bai1/bai1.ts
@@ -0,0 +1,15 @@
+class Person {
+    name: string;
+    age: number;
+    constructor(name: string, age: number) {
+        this.name = name;
+        this.age = age;
+    }
+
+    displayInformation(): void {
+        console.log(`Name: ${this.name} ` + `, Age: ${this.age}`);
+    }
+}
+
+const person = new Person("Anh", 20);
+person.displayInformation();
