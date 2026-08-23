87089c54aa61eb863cc28b70add80109faf34cf2
H82M19\Student <Student>
2026-08-23 17:05:24 +0700

    add: bai12

---
+++ Tuan1/bai12/bai12.ts
@@ -0,0 +1,26 @@
+interface Flyable {
+    fly(): void;
+}
+interface Swimmable {
+    swim(): void;
+}
+class Bird implements Flyable {
+    name: string;
+    constructor(name: string) {
+        this.name = name;
+    }
+    fly(): void {
+        console.log(`${this.name} is flying`);
+    }
+}
+class Fish implements Swimmable {
+    name: string;
+    constructor(name: string) {this.name = name;}
+    swim(): void {
+        console.log(`${this.name} is swimming`);
+    }
+}
+const bird = new Bird("Birddd");
+const fish = new Fish("Fishh")
+bird.fly();
+fish.swim();
